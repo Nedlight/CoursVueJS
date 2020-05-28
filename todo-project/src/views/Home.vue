c2f777d4a7531dd44dafc39e28d2e6d2652a1a13
Adrien Jeannier <adrien.jeannier@gmail.com>
2020-05-28 17:00:24 +0200

    VueJS + Vuetify

---
+++ todo-project/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <nav>
+        <v-toolbar flat app class="">
+          <v-toolbar-title class="text-uppercase grey--text ml-5">
+            <span class="font-weight-light">Todo</span>
+            <span>Project</span>
+          </v-toolbar-title>
+          <v-btn text color="grey">
+            <span>Sign out</span>
+            <v-icon>mdi-exit_to_app</v-icon>
+          </v-btn>
+        </v-toolbar>
+      </nav>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
